9075bc2e6ffb3094fd14e103be9be483dde2ec32
Dalton Durst <me@daltondurst.com>
2018-07-02 20:43:03 -0500

    Release SparkBot 0.3.0

--- changelog.rst
+++ changelog.rst
@@ -1,13 +1,12 @@
 Changelog
 =========
 
-0.1.0
+0.3.0
 -----
 
-* Add option to "yield" from a command to send messages to the user. This allows
-  for multiple replies with much simpler internal code than ``callback``
-  provided.
-* Deprecate ``callback``, to be removed in version 1.0.0
+* Add "Fallback" command, allowing for overriding the default behavior when a
+  command is not found
+* Add ability to remove the help command from the bot
 
 0.2.0
 -----
@@ -19,3 +18,11 @@ Changelog
 Existing users of SparkBot should be wary of this update. Check out the changes
 to run.py.example and the new Deploy documentation to learn about the needed
 conversions.
+
+0.1.0
+-----
+
+* Add option to "yield" from a command to send messages to the user. This allows
+  for multiple replies with much simpler internal code than ``callback``
+  provided.
+* Deprecate ``callback``, to be removed in version 1.0.0
--- setup.py
+++ setup.py
@@ -33,12 +33,12 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    version='0.3.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A simple chatbot for Cisco Spark',  # Required
+    description='A simple chatbot for Cisco Webex Teams',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
@@ -84,6 +84,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # You can just specify package directories manually here if your project is
